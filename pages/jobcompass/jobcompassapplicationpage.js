c49c296f8f2dded26fad9bb503e433cb1a209093
vaisakhsrinivas <vaisakh.srinivas@gmail.com>
2026-03-18 17:26:12 -0400

    adding more tests

--- pages/jobcompass/jobcompassapplicationpage.js
+++ pages/jobcompass/jobcompassapplicationpage.js
@@ -7,6 +7,18 @@ class applicationPage {
     async add_application() {
         await this.addApplication.click();
     }
-}
+
+    async add_application_with_details(jobTitle, companyName, jobLocation, salaryRange, jobUrl, notes) {
+        await this.addApplication.click();
+        await this.page.getByPlaceholder('Google').fill(companyName);
+        await this.page.locator('input[name=position]').fill(jobTitle);
+        await this.page.locator('input[name=location]').fill(jobLocation);
+        await this.page.locator('input[name=salary_range]').fill(salaryRange);
+        await this.page.locator('input[name=job_url]').fill(jobUrl);
+        await this.page.locator('textarea[name=notes]').fill(notes);
+        await this.page.locator('button[type="submit"]').click();
+
+    }
+    }
 
 module.exports = { applicationPage };
