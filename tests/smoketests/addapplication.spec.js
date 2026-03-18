08a394c1cfa6eb27cd0ff531fb2e31803cae0305
vaisakhsrinivas <vaisakh.srinivas@gmail.com>
2026-03-18 17:26:12 -0400

    adding more tests

--- tests/smoketests/addapplication.spec.js
+++ tests/smoketests/addapplication.spec.js
@@ -14,4 +14,13 @@ test.describe('Add Application', () => {
         await expect(page.getByText(testData[0].newapplicationlabel)).toBeVisible();
         await expect(page.getByText(testData[0].newapplicationlabel)).toHaveText(testData[0].newapplicationlabel);
     });
+    test('Add Application with Details Test', async ({ page }) => {
+        const loginPage = new LoginPage.loginPage(page);
+        await loginPage.goto();
+        await loginPage.login(testData[0].email, testData[0].password);
+        const applicationPage = new ApplicationPage.applicationPage(page);
+        await applicationPage.add_application_with_details(testData[1].jobTitle, testData[1].companyName, testData[1].jobLocation, testData[1].salaryRange, testData[1].jobUrl, testData[1].notes);
+        await expect(page.getByText(testData[1].jobTitle).first()).toBeVisible();
+        await expect(page.getByText(testData[1].companyName).first()).toBeVisible();
+    });
 });
