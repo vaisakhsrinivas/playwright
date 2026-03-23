d4d8f99c307834dc6bd0ed510dd839bb2737b669
vaisakhsrinivas <vaisakh.srinivas@gmail.com>
2026-03-23 16:54:54 -0400

    validating add application details and count

--- tests/smoketests/addapplication.spec.js
+++ tests/smoketests/addapplication.spec.js
@@ -1,6 +1,7 @@
 const {test, expect} = require('@playwright/test');
 const LoginPage = require('../../pages/jobcompass/jobcompassloginpage');
 const ApplicationPage = require('../../pages/jobcompass/jobcompassapplicationpage');
+const { count } = require('console');
 const testData = JSON.parse(JSON.stringify(require('../../data/jobcompass_testdata.json')));
 
 test.describe('Add Application', () => {
@@ -14,7 +15,8 @@ test.describe('Add Application', () => {
         await expect(page.getByText(testData[0].newapplicationlabel)).toBeVisible();
         await expect(page.getByText(testData[0].newapplicationlabel)).toHaveText(testData[0].newapplicationlabel);
     });
-    test('Add Application with Details Test', async ({ page }) => {
+    //commenting this test for now.
+    /*test('Add Application with Details Test', async ({ page }) => {
         const loginPage = new LoginPage.loginPage(page);
         await loginPage.goto();
         await loginPage.login(testData[0].email, testData[0].password);
@@ -23,6 +25,24 @@ test.describe('Add Application', () => {
         await applicationPage.add_application_with_details(title, testData[1].companyName, testData[1].jobLocation, testData[1].salaryRange, testData[1].jobUrl, testData[1].notes);
         await expect(page.getByText(title).first()).toBeVisible();
         await expect(page.getByText(testData[1].companyName).first()).toBeVisible();
+        
+    });*/
+    test('Application Details and Count Test', async ({ page }) => {
+        const loginPage = new LoginPage.loginPage(page);
+        await loginPage.goto();
+        await loginPage.login(testData[0].email, testData[0].password);
+        const applicationPage = new ApplicationPage.applicationPage(page);
+        const initialCount = parseInt(await page.locator('p:near(:text("Total Applications"))').textContent());
+        console.log('Initial application count:', initialCount);
+        const title = testData[1].jobTitle + '-' + generateRandomNumber();
+        await applicationPage.add_application_with_details(title, testData[1].companyName, testData[1].jobLocation, testData[1].salaryRange, testData[1].jobUrl, testData[1].notes);
+        await expect(page.getByText(title).first()).toBeVisible();
+        await expect(page.getByText(testData[1].companyName).first()).toBeVisible();
+        await page.locator('button:has-text("Dashboard")').click();
+        await page.reload();
+        const finalCount = parseInt(await page.locator('p:near(:text("Total Applications"))').textContent());
+        console.log('Final application count:', finalCount);
+        await expect(finalCount).toBe(initialCount+1);
         
     });
 });
