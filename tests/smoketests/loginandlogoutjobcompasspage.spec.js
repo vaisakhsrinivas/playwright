39dbb5c3ada3597309f2fa51d39f9d6ca49e690a
vaisakhsrinivas <vaisakh.srinivas@gmail.com>
2026-04-01 23:08:23 -0400

    invalid test added but commented out for now

--- tests/smoketests/loginandlogoutjobcompasspage.spec.js
+++ tests/smoketests/loginandlogoutjobcompasspage.spec.js
@@ -15,6 +15,15 @@ test.describe("Login and Logout",()=>{
         await expect(page).toHaveURL(testData[0].dashboardUrl)
     }
 )
+/* To be fixed: This test is failing because the error message is not visible on the page after an invalid login attempt. 
+ The test is trying to locate the error message using the text content, but it seems that the error message is not being rendered or is not visible in the DOM. This could be due to a variety of reasons such as incorrect selectors, timing issues, or changes in the application's UI. To fix this, we need to ensure that we are using the correct selectors to locate the error message and also consider adding some wait time to allow the error message to appear before asserting its visibility and text content.
+test.only("Invalid Login Test", async ({page})=>{
+    const loginPage = new LoginPage.loginPage(page)
+    await loginPage.goto()
+    await loginPage.login(testData[2].invalidEmail,testData[2].invalidPassword)
+    await expect(page.getByText(testData[2].invalidLoginMessage)).toBeVisible()
+    await expect(page.getByText(testData[2].invalidLoginMessage)).toHaveText(testData[2].invalidLoginMessage)
+})*/
 
     test("Logout Test", async ({page})=>{
         const loginPage = new LoginPage.loginPage(page)
