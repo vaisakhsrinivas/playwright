2e575e4bd22af62c25693cfdbb3deea0663a4af1
vaisakhsrinivas <vaisakh.srinivas@gmail.com>
2026-05-04 14:24:58 -0400

    adding page-object assertion helpers for the Swag Labs login use case in SwagLabsLoginPage.js

--- pages/swaglabs/SwagLabsLoginPage.js
+++ pages/swaglabs/SwagLabsLoginPage.js
@@ -1,9 +1,12 @@
+const { expect } = require('@playwright/test');
+
 class SwagLabsLoginPage {
     constructor(page) {
         this.page = page;
         this.username = page.locator('#user-name');
         this.password = page.locator('#password');
         this.loginButton = page.locator('input[type="submit"]');
+        this.errorMessageContainer = page.locator('.error-message-container');
     }
 
     async goto(url) {
@@ -14,6 +17,14 @@ class SwagLabsLoginPage {
         await this.username.fill(username);
         await this.password.fill(password);
         await this.loginButton.click();
+    }
+
+    async verifyLoginSuccess() {
+        await expect(this.page).toHaveURL(/inventory/);
+    }
+
+    async verifyLoginError() {
+        await expect(this.errorMessageContainer).toBeVisible();
     }
 }
 
--- tests/swaglabstests/loginpage.spec.js
+++ tests/swaglabstests/loginpage.spec.js
@@ -1,4 +1,4 @@
-const {test, expect} = require('@playwright/test');
+const {test} = require('@playwright/test');
 const testData = JSON.parse(JSON.stringify(require('../../data/swaglabs_testdata.json')));
 const SwagLabsLoginPage = require('../../pages/swaglabs/SwagLabsLoginPage');
 
@@ -7,14 +7,14 @@ test.describe('Swag Labs Login Tests', () => {
         const loginPage = new SwagLabsLoginPage(page);
         await loginPage.goto(testData[0].url);
         await loginPage.login(testData[0].username, testData[0].password);
-        await expect(page).toHaveURL(/inventory/);
+        await loginPage.verifyLoginSuccess();
     })
 
     test('Invalid Login Test', async ({ page }) => {
         const loginPage = new SwagLabsLoginPage(page);
         await loginPage.goto(testData[0].url);
         await loginPage.login(testData[0].username, testData[0].wrong_password);
-        await expect(page.locator('.error-message-container')).toBeVisible();
+        await loginPage.verifyLoginError();
     })
 
 
